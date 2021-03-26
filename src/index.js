977627b386a59c41c8cb834e61a16b16591ae2a3
Laura0629 <lvballen3@misena.edu.co>
2021-03-25 22:41:04 -0500

    Cambios

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Ejemplo from './Componentes/Ejemplo';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Ejemplo />
   </React.StrictMode>,
   document.getElementById('root')
 );
